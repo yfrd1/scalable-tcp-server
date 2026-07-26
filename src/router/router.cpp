aea7e5df1a4ee7a80559bcb2d54639caff439218
Younes <yfrdbit@gmail.com>
2026-07-26 12:39:54 +0330

    Add Router class

--- src/router/router.cpp
+++ src/router/router.cpp
@@ -1,9 +1,23 @@
 #include "router/router.hpp"
+#include "common/packet_enums.hpp"
+#include "common/packet.hpp"
+
+using scalable::common::PacketType;
 
 namespace scalable {
 namespace server {
 
-
+    void Router::route(Session& session, Packet& packet)
+    {
+        switch (packet.packet_type())
+        {
+        case PacketType::Auth:
+            
+        case PacketType::Account:
+        default:
+            break;
+        }
+    }
   
 
 }
--- src/router/router.hpp
+++ src/router/router.hpp
@@ -1,14 +1,38 @@
 #pragma once
 
+#include "handlers/auth_handler.hpp"
+#include "handlers/account_handler.hpp"
+#include "handlers/chat_handler.hpp"
+#include "handlers/file_handler.hpp"
+#include "handlers/stream_handler.hpp"
+
+#include "common/packet_enums.hpp"
+#include "session/session.hpp"
+
+using scalable::common::PacketType;
+
 namespace scalable {
 namespace server {
 
 class Router
 {
 public:
+    Router() = default;
+
+    Router(const Router&) = delete;
+    Router& operator = (const Router&) = delete;
+    
+    Router(Router&&) = delete;
+    Router& operator = (Router&&) = delete;
+
+    void route(Session& session, Packet& packet);
 
 private:
-
+    AuthHandler auth_handler_;
+    AccountHandler account_handler;
+    //ChatHandler chat_handler_;
+    //FileHandler file_handler_;
+    //StreamHandler stream_handler_;
 };
 
 }
